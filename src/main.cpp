850fdb32002a189351eb172e4043a9c93265d730
Bluubr <151577412+Bluubr@users.noreply.github.com>
2026-02-23 11:07:03 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,17 @@
 
 using namespace geode::prelude;
 
-// The Popup with the search bar
 class SearchMenuPopup : public Popup<std::string const&> {
 protected:
     bool setup(std::string const& value) override {
         this->setTitle("Workshop Search");
 
-        // Create search bar
-        auto searchBar = TextInput::create(200.f, "Search icons...");
+        auto searchBar = TextInput::create(200.f, "Search...");
         searchBar->setPosition(m_mainLayer->getContentSize() / 2);
         m_mainLayer->addChild(searchBar);
 
-        // Handle search logic here
         searchBar->setCallback([](auto const& text) {
-            log::info("User typed: {}", text);
+            log::info("Searching for: {}", text);
         });
 
         return true;
@@ -26,7 +23,6 @@ protected:
 public:
     static SearchMenuPopup* create() {
         auto ret = new SearchMenuPopup();
-        // Adjust width/height as needed
         if (ret && ret->initAnchored(250.f, 180.f, "")) {
             ret->autorelease();
             return ret;
@@ -36,40 +32,36 @@ public:
     }
 };
 
-// Hooking the Icon Kit
 class $modify(MyIconKit, CharacterColorPage) {
     bool init() {
         if (!CharacterColorPage::init()) return false;
 
-        // "buttons-menu" is the standard ID for the side buttons in Icon Kit
         auto menu = this->getChildByID("buttons-menu");
         if (!menu) return true; 
 
-        // Load your custom sprite using the _spr literal
-        // NOTE: workshop-icon.png must be in your mod's 'resources' folder
         auto btnSpr = CCSprite::create("workshop-icon.png"_spr);
         
-        // Scale it if it's too big/small
         if (btnSpr) {
             btnSpr->setScale(0.8f);
+        } else {
+            btnSpr = CCSprite::createWithSpriteFrameName("GJ_searchBtn_001.png");
         }
 
-        auto myBtn = CCMenuItemSpriteExtra::create(
+        auto searchBtn = CCMenuItemSpriteExtra::create(
             btnSpr,
             this,
-            menu_selector(MyIconKit::onOpenSearch)
+            menu_selector(MyIconKit::onOpenSearchMenu)
         );
 
-        menu->addChild(myBtn);
-        myBtn->setID("workshop-search-button"_spr);
+        menu->addChild(searchBtn);
+        searchBtn->setID("workshop-search-button"_spr);
         
-        // Refresh the layout so it aligns with other buttons
         menu->updateLayout();
 
         return true;
     }
 
-    void onOpenSearch(CCObject*) {
+    void onOpenSearchMenu(CCObject*) {
         SearchMenuPopup::create()->show();
     }
 };
